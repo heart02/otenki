2fa7d68a2d2f3d3326b7036c04b31f2ea3a1dc83
heart <heart@heart-no-Mac-mini.local>
2012-11-06 20:12:14 +0900

    Initial Commit

---
+++ otenki/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  otenki
+//
+//  Created by heart on 2012/11/06.
+//  Copyright (c) 2012年 heart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
